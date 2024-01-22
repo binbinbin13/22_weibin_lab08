89231ad9425be098ebb78ed9419dba6b486729b1
27-xinhui <122329129+27-xinhui@users.noreply.github.com>
2024-01-22 14:00:51 +0800

    Unity

--- 22_weibinnn_lab8/Assets/conversionScript.cs
+++ 22_weibinnn_lab8/Assets/conversionScript.cs
@@ -5,63 +5,87 @@ using UnityEngine.UI;
 
    public class conversionScript : MonoBehaviour
 {
-    
-        public Toggle USD;
-        public Toggle YEN;
-        private float SGDamount;
-        public InputField TxtAmount;
-        private float Value;
-        public InputField ValueTxt;
-        public Text DebugTxt;
-        // Start is called before the first frame update
-        void Start()
-        {
 
-        }
+    public Toggle USD;
+    public Toggle YEN;
+    public Toggle RM;
+    public Toggle EUR;
+    public Toggle KRW;
+    public Toggle TWD;
+    private float SGDamount;
+    public InputField TxtAmount;
+    private float Value;
+    public InputField ValueTxt;
+    public Text DebugTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
 
-        // Update is called once per frame
-        void Update()
-        {
-
-            if (USD.GetComponent<Toggle>().isOn == true && YEN.GetComponent<Toggle>().isOn == true)
-            {
-                DebugTxt.text = "Choose Either JPY or USD";
-            }
-        }
-        public void Conversion()
-        {
-
-            try
-            {
-
-                SGDamount = float.Parse(TxtAmount.text);
-                if (USD.GetComponent<Toggle>().isOn == true)
-                {
-                    Value = SGDamount * 0.74f;
-                    ValueTxt.text = Value.ToString() + " USD";
-                }
-                if (YEN.GetComponent<Toggle>().isOn == true)
-                {
-                    Value = SGDamount * 82.78f;
-                    ValueTxt.text = Value.ToString() + " JPY";
-                }
-                
-
-
-            }
-            catch (System.Exception)
-            {
-                DebugTxt.text = "Amount Empty";
-            }
-
-
-
-        }
-        public void ClearBtn()
-        {
-            YEN.GetComponent<Toggle>().isOn = false;
-            USD.GetComponent<Toggle>().isOn = false;
-            ValueTxt.text = "";
-            TxtAmount.text = "";
-        }
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    public void Conversion()
+    {
+
+        try
+        {
+
+            SGDamount = float.Parse(TxtAmount.text);
+            if (USD.GetComponent<Toggle>().isOn == true)
+            {
+                Value = SGDamount * 0.74f;
+                ValueTxt.text = Value.ToString() + " USD";
+            }
+            if (YEN.GetComponent<Toggle>().isOn == true)
+            {
+                Value = SGDamount * 82.78f;
+                ValueTxt.text = Value.ToString() + " JPY";
+            }
+            if (RM.GetComponent<Toggle>().isOn == true)
+            {
+                Value = SGDamount * 3.08f;
+                ValueTxt.text = Value.ToString() + " JPY";
+            }
+            if (EUR.GetComponent<Toggle>().isOn == true)
+            {
+                Value = SGDamount * 0.63f;
+                ValueTxt.text = Value.ToString() + " JPY";
+            }
+            if (KRW.GetComponent<Toggle>().isOn == true)
+            {
+                Value = SGDamount * 881.54f;
+                ValueTxt.text = Value.ToString() + " JPY";
+            }
+            if (TWD.GetComponent<Toggle>().isOn == true)
+            {
+                Value = SGDamount * 20.73f;
+                ValueTxt.text = Value.ToString() + " JPY";
+            }
+
+
+        }
+        catch (System.Exception)
+        {
+            DebugTxt.text = "Amount Empty";
+        }
+
+
+
+    }
+    public void ClearBtn()
+    {
+        YEN.GetComponent<Toggle>().isOn = false;
+        USD.GetComponent<Toggle>().isOn = false;
+        RM.GetComponent<Toggle>().isOn = false;
+        EUR.GetComponent<Toggle>().isOn = false;
+        KRW.GetComponent<Toggle>().isOn = false;
+        TWD.GetComponent<Toggle>().isOn = false;
+        ValueTxt.text = "";
+        TxtAmount.text = "";
+    }
+}
